57438de8a68200bf15ed14d9041060e1610aa698
harryg30 <gordenstein30@gmail.com>
2023-08-18 11:01:25 -0400

    Stations on map

--- harry-g-portfolio/src/components/BlueBike/Stations.js
+++ harry-g-portfolio/src/components/BlueBike/Stations.js
@@ -1,8 +1,24 @@
+import { useEffect, useState, useRef } from 'react'
 import { CircleMarker, Marker, Popup } from 'react-leaflet'
+import Papa from 'papaparse';
+import stationsFile from '../../assets/data/current_bluebikes_stations.json'
 
 const Stations = () => {
-    return(
-        <CircleMarker />
+
+
+    const stations = useState(stationsFile)
+
+
+
+
+
+    return (
+        <>{
+            stations.length === 0 || stations===undefined  ? <></> : stations[0].map(o => <CircleMarker center={[o.Latitude, o.Longitude]} key={o.id} />)
+        }
+        </>
+        
+
     )
 }
 
--- harry-g-portfolio/src/components/BlueBike/index.js
+++ harry-g-portfolio/src/components/BlueBike/index.js
@@ -19,7 +19,7 @@ const BlueBike = () => {
                             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
-                        {/* <Stations/> */}
+                        <Stations/>
                     </MapContainer>
                         : <></>}
                 </div>
