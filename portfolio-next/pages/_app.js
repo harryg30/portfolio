866423f60113af36d64670011ffaaab00af14cf7
harryg30 <gordenstein30@gmail.com>
2023-08-29 15:08:45 -0400

    trying to get leaflet set up

--- portfolio-next/pages/_app.js
+++ portfolio-next/pages/_app.js
@@ -1,6 +1,7 @@
 import Layout from '../app/layout'
 import '../styles/globals.scss'
 import React from 'react';
+import "leaflet/dist/leaflet.css";
 
 export default function App({ Component, pageProps }) {
   return (
--- portfolio-next/pages/bluebike/index.js
+++ portfolio-next/pages/bluebike/index.js
@@ -1,9 +1,10 @@
 import { useEffect, useState, useRef } from 'react'
 import React from 'react';
-import { MapContainer, TileLayer, CircleMarker, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-import {Stations} from './Stations'
 import "./index.module.scss"
+import dynamic from "next/dynamic"
+
+const Map = dynamic(() => import("./map"), { ssr:false })
 
 export default function BlueBike()  {
     const [center, setCenter] = useState([42.3342, -71.1041])
@@ -16,13 +17,7 @@ export default function BlueBike()  {
                     <h1>BlueBike rider data</h1>
                 </div>
                 <div  className='map-wrap'>
-                    {center != undefined ? <MapContainer center={center} zoom={12}>
-                        <TileLayer
-                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        />
-                        <Stations/>
-                    </MapContainer>
+                    {center != undefined ? <Map center={center}/>
                         : <></>}
                 </div>
             </div>
