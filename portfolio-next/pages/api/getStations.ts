dc4df97eef512c102a036a96d0684be74b4005c8
harryg30 <gordenstein30@gmail.com>
2023-09-11 14:41:36 -0400

    empty db message

--- portfolio-next/pages/api/getStations.ts
+++ portfolio-next/pages/api/getStations.ts
@@ -9,6 +9,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     }
     const stations = await prisma.station.findMany()
 
+    if(stations.length === 0){
+        return res.status(501).json({message: 'No Stations'})
+    }
+
     return res.status(200).json({stations})
 
 }
