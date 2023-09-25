d12ab1be2f81c89121de7c6732a3af85cd4b78ca
harryg30 <gordenstein30@gmail.com>
2023-09-25 16:13:39 -0400

    inline lables

--- portfolio-next/app/station/[number]/DeparturesByDay.tsx
+++ portfolio-next/app/station/[number]/DeparturesByDay.tsx
@@ -28,11 +28,6 @@ export default function DeparturesByDay({
         .attr("height", height)
         .attr("viewBox", [0, 0, width, height])
         .attr("style", "max-width: 100%; height: auto; height: intrinsic")
-        .style("-webkit-tap-highlight-color", "transparent")
-        .style("overflow", "visible")
-        .on("pointerenter pointermove", pointerMoved)
-        .on("pointerleave", pointerLeft)
-        .on("touchstart", event => event.preventDefault());
 
     // Add the x-axis.
     svg.append("g")
@@ -60,91 +55,27 @@ export default function DeparturesByDay({
         .attr("stroke", "steelblue")
         .attr("stroke-width", 1.5)
         .attr("d", line(data));
-
-    // Create the tooltip container.
-    const tooltip = svg.append("g");
-      
-      function formatDate(date) {
-        return date.toLocaleString("en", {
-          month: "short",
-          day: "numeric",
-          year: "numeric",
-          timeZone: "UTC"
-        });
-      }
-      
-      // Add the event listeners that show or hide the tooltip.
-      const bisect = d3.bisector(d => d.Date).center;
-      function pointerMoved(event) {
-        const i = bisect(data, x.invert(d3.pointer(event)[0]));
-        tooltip.style("display", null);
-        tooltip.attr("transform", `translate(${x(data[i].date)},${y(data[i].count)})`);
-    
-        const path = tooltip.selectAll("path")
-          .data([,])
-          .join("path")
-            .attr("fill", "white")
-            .attr("stroke", "black");
-    
-        const text = tooltip.selectAll("text")
-          .data([,])
-          .join("text")
-          .call(text => text
-            .selectAll("tspan")
-            .data([formatDate(data[i].date), data[i].count])
-            .join("tspan")
-              .attr("x", 0)
-              .attr("y", (_, i) => `${i * 1.1}em`)
-              .attr("font-weight", (_, i) => i ? null : "bold")
-              .text(d => d));
-    
-        size(text, path);
-      }
-    
-      function pointerLeft() {
-        tooltip.style("display", "none");
-      }
-    
-      // Wraps the text with a callout path of the correct size, as measured in the page.
-      function size(text, path) {
-        const {x, y, width: w, height: h} = text.node().getBBox();
-        text.attr("transform", `translate(${-w / 2},${15 - y})`);
-        path.attr("d", `M${-w / 2 - 10},5H-5l5,-5l5,5H${w / 2 + 10}v${h + 20}h-${w + 20}z`);
-      }
+    // inline counts
+    svg.append("g")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-linejoin", "round")
+        .attr("text-anchor", "middle")
+      .selectAll()
+      .data(data)
+      .join("text")
+        .text(d => d.count)
+        .attr("dy", "0.35em")
+        .attr("x", d => x(d.date))
+        .attr("y", d => y(d.count))
+        .attr("font-size", 10)
+        .clone(true).lower()
+        .attr("fill", "none")
+        .attr("stroke", "white")
+        .attr("stroke-width", 4);
 
 
     return (
         <svg id="departChart" />
     )
-
-
-
-
-
-    // const date: any[] = []
-    // const count: any[] = []
-
-    // for (const i in data) {
-    //     date.push(data[i]['date'])
-    //     count.push(data[i]['count'])
-    // }
-    // const gx = useRef();
-    // const gy = useRef();
-    // // Declare the x (vertical position) scale.
-    // const x = d3.scaleUtc([date[0], date[date.length - 1]], [marginLeft, width - marginRight])
-    // // Declare the y (vertical position) scale.
-    // const y = d3.scaleLinear(d3.extent(count), [height - marginBottom, marginTop]);
-    // const line = d3.line()
-    //     .x((d) => x(d['date']))
-    //     .y((d) => y(d['count']));
-
-    // return (
-    //     <svg width={width} height={height}>
-    //         <path fill="none" stroke="currentColor" strokeWidth="1.5" d={line(data)} />
-    //         <g fill="white" stroke="currentColor" strokeWidth="1.5">
-    //             {data.map((d, i) => (<circle key={i} cx={x(d['date'])} cy={y(d['count'])} r="2.5" />))}
-    //         </g>
-    //     </svg>
-    // );
 }
 
