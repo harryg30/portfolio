bd1703d9a20809682f17d7194303659a204f1c10
harryg30 <gordenstein30@gmail.com>
2023-09-29 10:31:32 -0400

    new error with dynamic page

--- portfolio-next/app/station/[number]/page.tsx
+++ portfolio-next/app/station/[number]/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 import { useEffect, useState } from 'react';
 import checkEnvironment from '../../../components/checkEnv';
 import DeparturesByDay from './DeparturesByDay';
